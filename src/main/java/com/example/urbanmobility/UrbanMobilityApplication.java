e3d702240449da281db3a3b709b99ed558b56cc8
rodercode <rodersnow@gmail.com>
2023-09-19 10:54:24 +0200

    Initial commit

---
+++ src/main/java/com/example/urbanmobility/UrbanMobilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.urbanmobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanMobilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanMobilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/urbanmobility/UrbanMobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urbanmobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanMobilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
