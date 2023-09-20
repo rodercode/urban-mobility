6a7499a033c40c748b9951573bd32c1cc205bf52
rodercode <rodersnow@gmail.com>
2023-09-20 11:11:41 +0200

    change it back so all tests are green during building process

--- src/test/java/com/example/urbanmobility/controller/AccountControllerEndToEndTest.java
+++ src/test/java/com/example/urbanmobility/controller/AccountControllerEndToEndTest.java
@@ -53,7 +53,7 @@ class AccountControllerEndToEndTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonAccount)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id", Matchers.is(1)))
                 .andExpect(jsonPath("$.username", Matchers.is("Roder")))
                 .andExpect(jsonPath("$.email", Matchers.is("Roder@example.com")));
