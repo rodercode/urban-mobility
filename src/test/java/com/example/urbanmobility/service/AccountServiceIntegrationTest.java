c04c35613d364f07bfe9526d87e05d6b3cfd25bc
rodercode <rodersnow@gmail.com>
2023-09-20 12:42:52 +0200

    make a test that check that the database does not have any account after delete user

--- src/test/java/com/example/urbanmobility/service/AccountServiceIntegrationTest.java
+++ src/test/java/com/example/urbanmobility/service/AccountServiceIntegrationTest.java
@@ -3,6 +3,8 @@ package com.example.urbanmobility.service;
 import com.example.urbanmobility.entity.Account;
 import com.example.urbanmobility.repository.AccountRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +28,13 @@ public class AccountServiceIntegrationTest {
     @Autowired
     AccountRepository accountRepository;
 
-    @Test
-    public void ShouldHaveSizeOne_WhenAccountIsSavedInDB(){
-        // Arrange
-        Account account = Account.builder()
+    // Variables
+    private Account account;
+
+    @DisplayName("Set up account data for each test")
+    @BeforeEach
+    public void setup(){
+        account = Account.builder()
                 .username("Roder")
                 .role("User")
                 .email("Roder@example.com")
@@ -38,9 +43,51 @@ public class AccountServiceIntegrationTest {
                 .paymentMethod("swish")
                 .isPaymentSet(true)
                 .build();
+    }
 
+    /*
+    Class: AccountService
+    Method: createAccount
+    Type of test: Integration test
+
+    Description: System should provide a way for a user to create a new account.
+
+    requirements
+    1. size should one when fetch all account after create a new account
+    2. save method in account Repository should not return null
+    */
+
+    @Test
+    public void ShouldHaveSizeOne_WhenAccountIsSavedInDB(){
+        // Act
         Account savedAccount = accountService.createAccount(account);
+
+        // Assert
         assertThat(savedAccount).isNotNull();
         assertThat(accountRepository.findAll().size()).isEqualTo(1);
+    }
+
+    /*
+    Class: AccountService
+    Method: deleteAccount
+    Type of test: Integration test
+
+    Description: System should provide a way for a user to delete their own account.
+
+    requirements
+    1. Database should be empty after delete a user by id
+    */
+
+    @Test
+    public void ShouldBeEmpty_AfterDeleteAccountById(){
+        // Arrange
+        accountRepository.save(account);
+        long accountId = account.getId();
+
+        // Act
+        accountService.deleteAccountById(accountId);
+
+        // Assert
+        assertThat(accountRepository.findAll().size()).isEqualTo(0);
     }
 }
