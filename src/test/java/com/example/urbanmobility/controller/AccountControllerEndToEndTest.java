97d2474c8d1d27eff0cd859511a6f5fd96ab33af
rodercode <rodersnow@gmail.com>
2023-09-20 13:38:41 +0200

    create a end to end test for delete feature, to check that is return correct status code, string message and that paramenter was entered

--- src/test/java/com/example/urbanmobility/controller/AccountControllerEndToEndTest.java
+++ src/test/java/com/example/urbanmobility/controller/AccountControllerEndToEndTest.java
@@ -1,8 +1,11 @@
 package com.example.urbanmobility.controller;
 import com.example.urbanmobility.entity.Account;
+import com.example.urbanmobility.repository.AccountRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +21,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -32,22 +34,33 @@ class AccountControllerEndToEndTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    // Variable
+    private Account account;
+    private String jsonAccount;
+
+    @BeforeEach
+    public void setup() throws JsonProcessingException {
+        account = Account.builder()
+                .username("Roder")
+                .role("User")
+                .email("Roder@example.com")
+                .phone("08123456789")
+                .paymentHistory(0)
+                .paymentMethod("swish")
+                .isPaymentSet(true)
+                .build();
+
+        accountRepository.save(account);
+        ObjectMapper mapper = new ObjectMapper();
+        jsonAccount = mapper.writeValueAsString(account);
+    }
+
     @Test
     @DisplayName("Test POST end point -> /api/account")
-    public void createAccountAPI() throws Exception {
-    Account account = Account.builder()
-            .username("Roder")
-            .role("User")
-            .email("Roder@example.com")
-            .phone("08123456789")
-            .paymentHistory(0)
-            .paymentMethod("swish")
-            .isPaymentSet(true)
-            .build();
-
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonAccount = mapper.writeValueAsString(account);
-
+    public void CheckEndPoint_OfCreateAccount() throws Exception {
         mvc.perform(MockMvcRequestBuilders
                 .post("/api/account")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -58,5 +71,14 @@ class AccountControllerEndToEndTest {
                 .andExpect(jsonPath("$.username", Matchers.is("Roder")))
                 .andExpect(jsonPath("$.email", Matchers.is("Roder@example.com")));
 
+    }
+
+    @Test
+    @DisplayName("Test Delete end point -> /api/delete/{id}")
+    public void createAccountAPI() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .delete("/api/account/{id}", 1L))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("Account was deleted successfully"));
     }
 }
